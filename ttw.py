93fba5ca39dc19aa79498104d850981cc64a5418
philip-mv <philipmv10@gmail.com>
2025-02-08 16:05:51 +0530

    yihcijvjk gjg kj.

--- ttw.py
+++ ttw.py
@@ -1,6 +1,6 @@
 def tc(bill_amount,tip_perc):
     total = (bill_amount*tip_perc /100) + bill_amount
-    print(f"Please pay rupees{total}")
+    print(f"Please pay rupees : {total}")
 
 
 bill_amount = int(input("Please enter the bill amount : "))
